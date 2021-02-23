3b6a72d58cb5042d11836f13b913a5aecbc91ea8
Asmae Ouajjani <hajarouajjani@gmail.com>
2021-02-23 11:49:23 +0100

    hello girls

--- src/application/AdminElevController.java
+++ src/application/AdminElevController.java
@@ -1,7 +1,12 @@
 package application;
 
 import java.io.IOException;
+import java.time.LocalDate;
+import java.time.Month;
 
+import classProjet.Eleve;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -9,6 +14,9 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
@@ -76,5 +84,61 @@ public class AdminElevController {
 	    public void Minus(MouseEvent mouseEvent) {
 	        Stage stage = (Stage) ((Node) mouseEvent.getSource()).getScene().getWindow();
 	        stage.setIconified(true);
+	    }
+	    
+	    
+	    @FXML
+	    private TableView<Eleve> tableView;
+
+	    @FXML
+	    private TableColumn<Eleve, Integer> IdEColumn;
+
+	    @FXML
+	    private TableColumn<Eleve, String> NomColumn;
+
+	    @FXML
+	    private TableColumn<Eleve, String> PrenomColumn;
+
+	    @FXML
+	    private TableColumn<Eleve, LocalDate> DateDeNaissanceColumn;
+
+	    @FXML
+	    private TableColumn<Eleve, String> EmailColumn;
+
+	    @FXML
+	    private TableColumn<Eleve, Integer> NombreAbsColumn;
+
+	    @FXML
+	    private TableColumn<Eleve, Double> NoteS1Column;
+
+	    @FXML
+	    private TableColumn<Eleve, Double> NoteS2Column;
+
+	    @FXML
+	    private TableColumn<Eleve, Double> NoteTotaleColumn;
+	    
+	    public void initialize() {
+	    	//set up the columns in the table
+	    	IdEColumn.setCellValueFactory(new PropertyValueFactory<Eleve,Integer>("idE"));
+	    	NomColumn.setCellValueFactory(new PropertyValueFactory<Eleve,String>("nom"));
+	    	PrenomColumn.setCellValueFactory(new PropertyValueFactory<Eleve,String>("prenom"));
+	    	DateDeNaissanceColumn.setCellValueFactory(new PropertyValueFactory<Eleve,LocalDate>("dateDeNaissance"));
+	    	EmailColumn.setCellValueFactory(new PropertyValueFactory<Eleve,String>("email"));
+	    	NombreAbsColumn.setCellValueFactory(new PropertyValueFactory<Eleve,Integer>("nombreAbs"));
+	    	NoteS1Column.setCellValueFactory(new PropertyValueFactory<Eleve,Double>("noteS1"));
+	    	NoteS2Column.setCellValueFactory(new PropertyValueFactory<Eleve,Double>("noteS2"));
+	    	NoteTotaleColumn.setCellValueFactory(new PropertyValueFactory<Eleve,Double>("noteTotale"));
+	    	//load some data
+	    	tableView.setItems(getEleve());
+	    }
+	    
+	    public ObservableList<Eleve> getEleve(){
+			
+	    	ObservableList<Eleve> eleve = FXCollections.observableArrayList();
+	    	eleve.add(new Eleve(12,0,"hajar","ouajjani","hajarouajjani@gmail.com",LocalDate.of(1997, Month.FEBRUARY, 16),16,15.5,16));
+	    	eleve.add(new Eleve(13,2,"nada","essalhy","nadaessalhy@gmail.com",LocalDate.of(1997, Month.AUGUST, 14),17,18,17.5));
+	    	return eleve;
+	    	
+	    	
 	    }
 }

775e6d6f2a669ca4b0a92391763d43269b604457
nada2019 <nada.essalehy@gmail.com>
2021-02-25 17:03:36 +0100

    add amploi changes

--- src/application/Main.java
+++ src/application/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
 	 @Override
 	public void start(Stage primaryStage) throws  Exception{
 		
-		 Parent root = FXMLLoader.load(getClass().getResource("emploi.fxml"));
+		 Parent root = FXMLLoader.load(getClass().getResource("Choix.fxml"));
           Scene scene = new Scene (root);
 		
 		 primaryStage.setScene(scene);
